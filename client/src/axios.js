774d0ea98eea270212439ecd94197e28f1375255
Gaurav r-a-j <gk5271@srmist.edu.in>
2022-12-31 19:41:16 +0530

    updated link

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5500/api"
+// const BASE_URL = "http://localhost:5500/api"
+const BASE_URL = "https://qsearch.onrender.com/api"
 
 
 
