6749c809ba2879031514c9b6b67fee9df289d50f
Gaurav-r-a-j <gk5271@srmist.edu.in>
2023-05-14 20:27:00 +0530

    update privacy policy content

--- client/src/pages/FooterPages/PrivacyPolicy.jsx
+++ client/src/pages/FooterPages/PrivacyPolicy.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import styles from './footerpages.module.css' 
+import styles from "./footerpages.module.css";
 
 const PrivacyPolicy = () => {
   useEffect(() => {
@@ -12,190 +12,66 @@ const PrivacyPolicy = () => {
         <div
           className={`${styles.policyTexts}  d-flex-center pdl fd-col gapl `}
         >
+          <b>
+            At QSearch, we value your privacy and are committed to protecting
+            your personal information. This Privacy Policy outlines how we
+            collect, use, and protect the information you provide to us when you
+            use our online printing services.
+          </b>
+          <b>Information We Collect:</b>
+          We collect personal information from you when you register on our
+          website, place an order, or communicate with us via email or live
+          chat. This information may include your name, address, phone number,
+          email address, and payment information.
+          <b>How We Use Your Information:</b>
           <p>
-            This privacy policy covers the practices for handling and securing
-            your personal information by SWNAC E KIRAYA SERVICES PRIVATE LIMITED
-            (Hereinafter Referred to as &quot;SharePal&quot;). The domain name
-            www.sharepal.in (hereinafter referred to as the
-            &quot;Website&quot;), is owned by SWNAC E KIRAYA SERVICES PRIVATE
-            LIMITED, a Company incorporated under the Companies Act, 2013 having
-            its registered office at 1003/14 Sadar Road, Ambikapur, Surguja,
-            Chhattisgarh, India 497001.
+            We use the information we collect from you to process your orders
+            and provide you with high-quality printing services. We may also use
+            your information to improve our website, customer service, and
+            marketing efforts.
           </p>
-
           <p>
-            This Privacy Policy is applicable to persons who purchase or rent
-            any product or services from SharePal through any sales channel of
-            SharePal (like website, mobile site, telephonic order & offline
-            channels). This Privacy Policy also applies to persons who visit
-            SharePal&apos;s offices. As a user or customer of SharePal, the
-            person agrees with this privacy policy and the terms and conditions
-            mentioned herein.
+            We do not sell, trade, or otherwise transfer your personal
+            information to third parties without your consent, except as
+            required by law or as necessary to provide our services to you. We
+            may share your information with our trusted third-party service
+            providers who assist us in operating our website, conducting our
+            business, or servicing you.
           </p>
-
           <p>
-            When browsing our Website, you are not required to provide any
-            Personal Information unless and until you choose to make a
-            purchase/rent or sign up for one of our email newsletters or other
-            services as described below.
+            We use standard security measures to protect your personal
+            information from unauthorized access, use, or disclosure. We also
+            implement a variety of security measures to maintain the safety of
+            your personal information when you place an order or enter, submit,
+            or access your personal information.
           </p>
-
-          <b>COLLECTION OF PERSONAL INFORMATION</b>
-
+          <b>Cookies:</b>
           <p>
-            For availing of the services/products from the website or to contact
-            SharePal, you may have to sign up by giving the following personal
-            information such as email address, name, phone number and address.
-            Some of the information provided above is personally sensitive
-            information (“Personal Information”) and are collected for the
-            purpose of verification of the identity of the user and to ensure
-            compliances. We do use your contact information to send you offers
-            based on your previous orders and your interests. In general, you
-            can browse the Website without telling us who you are or revealing
-            any Personal Information about yourself. Additionally, if you browse
-            through this Website, our servers may automatically record
-            information of a certain kind. Such information includes information
-            such as the name of the domain and host from which you access the
-            Internet; the Internet Protocol (IP) address of the computer you are
-            using; the date and time of your access. We use this information to
-            monitor the usage of our Website and also whatever is necessary for
-            our business. This information does not include Personal
-            Information. If you choose to post messages on our message boards,
-            chat rooms or other message areas or leave feedback, we will collect
-            the information you provide to us. We retain this information as
-            necessary to resolve disputes, provide customer support and
-            troubleshoot problems as permitted by law. If you send us personal
-            correspondence, such as emails or letters, or if other users or
-            third parties send us correspondence about your activities or
-            postings on the Website, we may collect such information into a file
-            specific to you.
+            We use cookies to enhance your browsing experience and to
+            personalize our services for you. Cookies are small text files that
+            are stored on your computer or mobile device when you visit our
+            website. You may choose to disable cookies through your browser
+            settings, but this may affect your ability to use certain features
+            of our website.
           </p>
-
-          <b>COOKIES</b>
-
+          <b>Third-Party Links:</b>
           <p>
-            Cookies are small pieces of information that are stored by your
-            browser on your device&apos;s hard drive. Cookies are only read by
-            the server that placed them, and are unable to do such things as run
-            programs on your computer, plant viruses or harvest your Personal
-            Information. The use of cookies is very common on the Internet.
-            <br />
-            SharePal&apos;s use of cookies is similar to that of any other
-            reputable online companies. No personally identifiable information
-            (PII) about you (e.g., name, address, etc.) is gathered or stored in
-            the cookies placed by a website or mobile site and, as a result,
-            none can be passed on to any third parties. Cookies allow us to
-            serve you better and more efficiently, and to personalize your
-            experience at our website and mobile site. SharePal uses cookies to
-            personalize your experience on the website and mobile site, and with
-            respect to advertisements.
-            <br />
-            A cookie may also be placed by our advertising server. Such cookies
-            are used only for purposes of tracking the effectiveness of
-            advertising served by us on our website and mobile site and no PII
-            is gathered from you by the use of these cookies, nor is this
-            information shared with any third parties. Similarly, a cookie may
-            be placed by our third-party advertising companies or advertisement
-            providers or servers. These companies may use aggregated statistics
-            about your visits to our websites or mobile sites in order to
-            provide advertisements about related goods and services that you may
-            be interested in or any goods and services of potential interest to
-            you. The information they collect does not include your PII.
-            <br />
-            When a user sends personal correspondence, such as emails, letters,
-            messages on the Site&apos;s Facebook Page, Instagram Page and/ or
-            other social media platforms on which the App / Website Owner has an
-            account and/ or calls the App /Website Owner&apos;s Customer Care
-            Cell/ Grievance Officer, the App/Website Owner retains this
-            information. This information may be used by the App/Website Owner
-            and/ or maybe published in accordance with the provisions of the
-            Terms of Use of this App and this Privacy Policy.
-            <br />
-            The third-party advertising companies or advertisement providers may
-            also employ technology that is used to measure the effectiveness of
-            ads. Any such information is anonymous. They may use this anonymous
-            information about your visits to our websites in order to provide
-            advertisements about goods and services of potential interest to
-            you. No PII is collected during this process. The information is
-            anonymous and does not link online actions to an identifiable
-            person.
-            <br />
-            Most Web browsers automatically accept cookies. Of course, by
-            changing the options on your web browser or using certain software
-            programs, you can control how and whether cookies will be accepted
-            by your browser. SharePal supports your right to block any unwanted
-            Internet activity, especially that of unscrupulous websites.
-            However, blocking SharePal cookies may disable certain features on
-            the website and mobile site, and may make it impossible to purchase
-            or use certain services available on the website and mobile site.
-            Please note that it is possible to block cookie activity from
-            certain websites while permitting cookies from websites you trust.
-            <br />
-            However, we do not sell or rent individual customer names or other
-            Personal Information to third parties except sharing such
-            information with our alliance partners or vendors who are engaged by
-            us for providing various promotional and other benefits to our
-            customers from time to time basis their booking history with us.
-            <br />
-            We may also share certain filtered Personal Information to our
-            business partners who may contact the customers to offer certain
-            products or services (which may include free or paid
-            products/services) which will enable the customer to have a better
-            or aligned renting experience or to avail certain benefits specially
-            made for SharePal Customers. Examples of such partners are entities
-            offering co-branded credit cards, banking cards or similar sensitive
-            information etc.
-            <br />
-            We use non - personally identifiable information in aggregate form
-            to build higher quality, more useful online services by performing
-            statistical analysis of the collective characteristics and behaviour
-            of our customers and visitors, and by measuring demographics and
-            interests regarding specific areas of the website, mobile site and
-            mobile app. We may provide anonymous statistical information based
-            on this data to suppliers, advertisers, affiliates and other current
-            and potential Business partners. We may also use such aggregate data
-            to inform these third-parties as to the number of people who have
-            seen and clicked on links to their websites.
-            <br />
-            Occasionally, SharePal will hire a third party to act on our behalf
-            for projects such as market -research surveys and contest - entry
-            processing and will provide information to these third parties
-            specifically for use in connection with these projects. The
-            information (including aggregate cookie and tracking information) we
-            provide to such third parties, alliance partners, or vendors are
-            protected by confidentiality agreements and such information is to
-            be used solely for completing the specific project, and in
-            compliance with the applicable regulations.
-            <br />
-            All the payments on the website, mobile site and mobile app are
-            secured via 128-Bit Encryption via an SSL Certificate obtained from
-            an authority in selling these products.
-            <br />
-            This means all Personal Information you provide is transmitted using
-            SSL (Secure Socket Layer) encryption. SSL is a proven coding system
-            that lets your browser automatically encrypt, or scramble, data
-            before you send it to us.
+            Our website may contain links to third-party websites that have
+            their own privacy policies. We are not responsible for the privacy
+            practices or content of these third-party websites.
           </p>
-
-          <b>PAYMENT GATEWAY:</b>
+          <b>Changes to Our Privacy Policy:</b>
           <p>
-            Information relating to electronic transactions entered into via the
-            Website shall be protected by encryption technology. We have
-            partnered with secure payment gateways i.e. Razorpay. The Website
-            does not have the ability to interfere and do not interfere with the
-            payment gateway mechanism. The Website has no access to the
-            information that you may enter for making the payment through the
-            payment gateway. Your transaction and banking details or other
-            information as required for internet banking or other payment
-            instruments is held by our Payment Gateway partner. By creating a
-            link to a payment gateway, we do not endorse the payment gateway,
-            nor are we liable for any failure of products or services offered by
-            such payment gateway. Such a payment gateway may have a privacy
-            policy different from that of ours. All failures/ errors/ omissions
-            of the payment gateway shall be solely on the payment gateway. You
-            hereby consent that you shall not use the Website for any disputes
-            that you may have with the payment gateway for any wrongdoing of the
-            payment gateway.
+            We reserve the right to modify this Privacy Policy at any time. If
+            we make changes to our Privacy Policy, we will notify you by email
+            or by posting the updated policy on our website.
+          </p>
+          <b>Contact Us:</b>
+          <p>
+            If you have any questions or concerns about our Privacy Policy,
+            please contact us at support@qsearch.com. We are committed to
+            protecting your privacy and will do our best to address any issues
+            or concerns you may have.
           </p>
         </div>
       </div>
